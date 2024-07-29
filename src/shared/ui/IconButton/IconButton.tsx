131e2e5226d7bc11171efc68d2bd7249a04d1243
Kirill <kirillbar24@gmail.com>
2024-07-29 21:17:07 +0400

    added iconButton

---
+++ src/shared/ui/IconButton/IconButton.tsx
@@ -0,0 +1,16 @@
+import { FC, ReactNode } from 'react';
+import clsx from 'clsx';
+import styles from './IconButton.module.scss';
+
+interface IIconButtonProps {
+    icon: ReactNode;
+    className: string;
+}
+
+export const IconButton: FC<IIconButtonProps> = ({ icon, className }) => {
+    return (
+        <button type={'button'} className={clsx(styles.iconButton, className)}>
+            {icon}
+        </button>
+    );
+};
