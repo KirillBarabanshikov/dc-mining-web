41e858069c3abf99d95e0b507011bf4b9845cd4c
Kirill <kirillbar24@gmail.com>
2024-07-29 21:15:51 +0400

    search input v1

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,12 +1,53 @@
-import { FC, InputHTMLAttributes } from 'react';
+import React, { FC, InputHTMLAttributes, useRef, useState } from 'react';
+import clsx from 'clsx';
+import SearchIcon from '@/shared/assets/icons/search.svg?react';
+import CloseIcon from '@/shared/assets/icons/close.svg?react';
 import styles from './Input.module.scss';
 
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-export const Input: FC<IInputProps> = ({ type = 'text', ...props }) => {
+export const Input: FC<IInputProps> = ({
+    type = 'text',
+    className,
+    ...props
+}) => {
+    const [value, setValue] = useState('');
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    function handleInputFocus() {
+        if ('focus' in inputRef.current) {
+            inputRef.current.focus();
+        }
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+    };
+
+    const clearInput = () => {
+        setValue('');
+        if ('focus' in inputRef.current) {
+            inputRef.current.focus();
+        }
+    };
+
     return (
-        <div className={styles.input}>
-            <input type={type} {...props} />
+        <div
+            className={clsx(styles.input, className)}
+            onClick={handleInputFocus}
+        >
+            <input
+                type={type}
+                ref={inputRef}
+                value={value}
+                onChange={handleChange}
+                {...props}
+            />
+            {type === 'search' && value ? (
+                <CloseIcon onClick={clearInput} />
+            ) : (
+                <SearchIcon />
+            )}
         </div>
     );
 };
