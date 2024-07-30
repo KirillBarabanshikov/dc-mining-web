a95e4eb69b7c4de888f39885272d8719087c54a8
Kirill <kirillbar24@gmail.com>
2024-07-30 13:29:46 +0400

    fix main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import clsx from 'clsx';
 import { Bestsellers, MainBanner, Offers } from '@/widgets';
 import { Advantages } from '@/widgets/Advantages';
-
+import BackgroundIcon from '@/shared/assets/icons/backgrounds/main-bg.svg?react';
+import styles from './MainPage.module.scss';
 export const MainPage = () => {
     return (
-        <div>
+        <>
             <MainBanner />
-            <Advantages as={'main'} />
-            <Offers />
-            <Bestsellers />
-        </div>
+            <div className={clsx(styles.sections, 'sections')}>
+                <Advantages as={'main'} />
+                <Offers />
+                <div className={styles.bestsellersWrapper}>
+                    <Bestsellers />
+                    <BackgroundIcon className={styles.backgroundIcon} />
+                </div>
+            </div>
+        </>
     );
 };
