b4c6f17efdf05e0b2b6099ada51f296b163e9772
Kirill <kirillbar24@gmail.com>
2024-07-30 10:18:56 +0400

    responsive header

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,8 +1,9 @@
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { IconButton, Input } from '@/shared/ui';
-import clsx from 'clsx';
 import { HorizontalMenu } from '@/widgets';
+import { useMediaQuery } from '@/shared/lib';
+import { MAX_WIDTH_LG, MAX_WIDTH_MD } from '@/shared/consts';
 import Logo from '@/shared/assets/logo.svg?react';
 import BurgerIcon from '@/shared/assets/icons/burger.svg?react';
 import HeartIcon from '@/shared/assets/icons/heart.svg?react';
@@ -10,47 +11,66 @@ import StatisticIcon from '@/shared/assets/icons/statistic.svg?react';
 import TelegramIcon from '@/shared/assets/icons/telegram.svg?react';
 import WhatsappIcon from '@/shared/assets/icons/whatsapp.svg?react';
 import PhoneIcon from '@/shared/assets/icons/phone.svg?react';
+import SearchIcon from '@/shared/assets/icons/search.svg?react';
 import styles from './Header.module.scss';
 
 export const Header: FC = () => {
+    const matchesLG = useMediaQuery(MAX_WIDTH_LG);
+    const matchesMD = useMediaQuery(MAX_WIDTH_MD);
+
     return (
-        <header className={clsx(styles.header)}>
-            <div className={styles.mainMenu}>
-                <IconButton icon={<BurgerIcon />} />
-                <Link to={'/'} className={styles.logo}>
-                    <Logo />
-                </Link>
-                <Input
-                    type={'search'}
-                    placeholder={'Поиск'}
-                    className={styles.search}
-                />
-                <div className={styles.options}>
-                    <div className={styles.option}>
-                        <HeartIcon />
-                        <span>Избранное</span>
-                    </div>
-                    <div className={styles.option}>
-                        <StatisticIcon />
-                        <span>Сравнить</span>
-                    </div>
-                    <div className={styles.contacts}>
-                        <div className={styles.option}>
-                            <TelegramIcon />
-                            <span>Telegram</span>
-                        </div>
-                        <div className={styles.option}>
-                            <WhatsappIcon />
-                            <span>Whatsapp</span>
-                        </div>
-                        <div className={styles.option}>
-                            <PhoneIcon />
-                            <span>Заказать звонок</span>
-                        </div>
+        <header className={styles.header}>
+            <div className={styles.headerContainer}>
+                <div className={styles.mainMenu}>
+                    <IconButton
+                        icon={<BurgerIcon />}
+                        className={styles.menuIcon}
+                    />
+                    <Link to={'/'} className={styles.logo}>
+                        <Logo />
+                    </Link>
+                    {!matchesLG && (
+                        <Input
+                            type={'search'}
+                            placeholder={'Поиск'}
+                            className={styles.search}
+                        />
+                    )}
+                    <div className={styles.options}>
+                        {matchesLG && (
+                            <div className={styles.option}>
+                                <SearchIcon />
+                                <span>Поиск</span>
+                            </div>
+                        )}
+                        <NavLink to={''} className={styles.option}>
+                            <HeartIcon />
+                            <span>Избранное</span>
+                        </NavLink>
+                        <NavLink className={styles.option}>
+                            <StatisticIcon />
+                            <span>Сравнить</span>
+                        </NavLink>
+                        {!matchesMD && (
+                            <div className={styles.contacts}>
+                                <div className={styles.option}>
+                                    <TelegramIcon />
+                                    <span>Telegram</span>
+                                </div>
+                                <div className={styles.option}>
+                                    <WhatsappIcon />
+                                    <span>Whatsapp</span>
+                                </div>
+                                <div className={styles.option}>
+                                    <PhoneIcon />
+                                    <span>Заказать звонок</span>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
+                {!matchesMD && <HorizontalMenu />}
             </div>
-            <HorizontalMenu />
         </header>
     );
 };
--- src/widgets/HorizontalMenu/HorizontalMenu.tsx
+++ src/widgets/HorizontalMenu/HorizontalMenu.tsx
@@ -3,8 +3,12 @@ import { Link } from 'react-router-dom';
 import { horizontalMenuItems } from './data/data.ts';
 import { MenuItemDropdown } from './components/MenuItemDropdown.tsx';
 import styles from './HorizontalMenu.module.scss';
+import { useMediaQuery } from '@/shared/lib';
+import { MAX_WIDTH_LG } from '@/shared/consts';
 
 export const HorizontalMenu = () => {
+    const matches = useMediaQuery(MAX_WIDTH_LG);
+
     return (
         <nav className={styles.horizontalMenu}>
             <ul className={styles.horizontalMenuList}>
@@ -13,7 +17,7 @@ export const HorizontalMenu = () => {
                         return <MenuItemDropdown key={index} item={item} />;
                     }
 
-                    if ('path' in item) {
+                    if ('path' in item && !matches) {
                         return (
                             <li
                                 key={index}
