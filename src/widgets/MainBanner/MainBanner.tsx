674bc0ae6a7a820b4ac1c4499cf1414971996139
Kirill <kirillbar24@gmail.com>
2024-07-30 08:04:14 +0400

    fix

--- src/widgets/MainBanner/MainBanner.tsx
+++ src/widgets/MainBanner/MainBanner.tsx
@@ -7,22 +7,20 @@ import styles from './MainBanner.module.scss';
 
 export const MainBanner = () => {
     return (
-        <>
-            <section className={clsx(styles.banner, 'container')}>
-                <div>
-                    <h1>Размещение вашего оборудования в дата центре</h1>
-                    <ul>
-                        <li>Официальное размещение от 4,7 руб/кВт</li>
-                        <li>Вооруженная охрана</li>
-                        <li>Профессиональный сервис-центр</li>
-                        <li>Более 1000 устройств в обслуживании</li>
-                    </ul>
-                    <Button className={styles.button}>Подробнее</Button>
-                    <div></div>
-                </div>
-                <img src={`${miner}`} alt={'miner'} />
-            </section>
+        <section className={clsx(styles.banner, 'container')}>
+            <div>
+                <h1>Размещение вашего оборудования в дата центре</h1>
+                <ul>
+                    <li>Официальное размещение от 4,7 руб/кВт</li>
+                    <li>Вооруженная охрана</li>
+                    <li>Профессиональный сервис-центр</li>
+                    <li>Более 1000 устройств в обслуживании</li>
+                </ul>
+                <Button className={styles.button}>Подробнее</Button>
+                <div></div>
+            </div>
+            <img src={`${miner}`} alt={'miner'} />
             <img src={`${bg}`} alt={'Main banner'} className={styles.cover} />
-        </>
+        </section>
     );
 };
