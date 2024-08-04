b88032fabb8a1cfc5f7099090ed590e0bd4661b6
Kirill <kirillbar24@gmail.com>
2024-08-04 19:44:43 +0400

    fix product card

--- src/entities/product/ui/ProductCard/ProductCard.tsx
+++ src/entities/product/ui/ProductCard/ProductCard.tsx
@@ -1,17 +1,41 @@
-import img from '@/widgets/Bestsellers/img.png';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import clsx from 'clsx';
 import { Badge, Button, IconButton } from '@/shared/ui';
 import HeartIcon from '@/shared/assets/icons/heart2.svg?react';
 import StatisticIcon from '@/shared/assets/icons/statistic2.svg?react';
+import img from '@/widgets/Bestsellers/img.png';
+import img2 from './img.png';
 import styles from './ProductCard.module.scss';
-import { useNavigate } from 'react-router-dom';
+
+const images = [img, img2, img, img2];
 
 export const ProductCard = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [isFavorite, setIsFavorite] = useState(false);
     const navigate = useNavigate();
 
     return (
         <article className={styles.productCard} onClick={() => navigate('/product')}>
             <div className={styles.image}>
-                <img src={`${img}`} alt={'product'} />
+                <img src={`${images[currentSlide]}`} alt={'product'} />
+                <div className={styles.slides}>
+                    {images.map((_, index) => {
+                        return (
+                            <span key={index} onMouseEnter={() => setCurrentSlide(index)} className={styles.slide} />
+                        );
+                    })}
+                </div>
+                <div className={styles.pagination}>
+                    {images.map((_, index) => {
+                        return (
+                            <span
+                                key={index}
+                                className={clsx(styles.bullet, index === currentSlide && styles.active)}
+                            />
+                        );
+                    })}
+                </div>
             </div>
             <div className={styles.tags}>
                 <Badge text={'В наличии'} theme={'green'} />
@@ -30,8 +54,22 @@ export const ProductCard = () => {
                 <Button size={'sm'} className={styles.button}>
                     Заказать
                 </Button>
-                <IconButton icon={<HeartIcon />} className={styles.iconButton} />
-                <IconButton icon={<StatisticIcon />} className={styles.iconButton} />
+                <IconButton
+                    icon={<HeartIcon />}
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        setIsFavorite(!isFavorite);
+                    }}
+                    className={clsx(styles.iconButton, isFavorite && styles.isFavorite)}
+                />
+                <IconButton
+                    icon={<StatisticIcon />}
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        setIsFavorite(!isFavorite);
+                    }}
+                    className={clsx(styles.iconButton, isFavorite && styles.isFavorite)}
+                />
             </div>
         </article>
     );
--- src/shared/ui/IconButton/IconButton.tsx
+++ src/shared/ui/IconButton/IconButton.tsx
@@ -1,24 +1,15 @@
-import { FC, ReactNode } from 'react';
+import { ButtonHTMLAttributes, FC, ReactNode } from 'react';
 import clsx from 'clsx';
 import styles from './IconButton.module.scss';
 
-interface IIconButtonProps {
+interface IIconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     icon: ReactNode;
     className?: string;
-    onClick?: () => void;
 }
 
-export const IconButton: FC<IIconButtonProps> = ({
-    icon,
-    className,
-    onClick,
-}) => {
+export const IconButton: FC<IIconButtonProps> = ({ icon, className, onClick }) => {
     return (
-        <button
-            type={'button'}
-            onClick={onClick}
-            className={clsx(styles.iconButton, className)}
-        >
+        <button type={'button'} onClick={onClick} className={clsx(styles.iconButton, className)}>
             {icon}
         </button>
     );
