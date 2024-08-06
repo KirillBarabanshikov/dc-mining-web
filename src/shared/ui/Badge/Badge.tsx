1cb1f770b131c8b529ba2f32a4aa0cdeeda93f33
Kirill <kirillbar24@gmail.com>
2024-08-06 17:05:09 +0300

    fixed badge

--- src/shared/ui/Badge/Badge.tsx
+++ src/shared/ui/Badge/Badge.tsx
@@ -4,9 +4,19 @@ import styles from './Badge.module.scss';
 
 interface IBadgeProps {
     text: string;
-    theme: 'green' | 'red' | 'blue';
+    color: string;
 }
 
-export const Badge: FC<IBadgeProps> = ({ text, theme }) => {
-    return <span className={clsx(styles.badge, styles[theme])}>{text}</span>;
+export const Badge: FC<IBadgeProps> = ({ text, color }) => {
+    return (
+        <span className={clsx(styles.badge)}>
+            <span aria-hidden={true} className={clsx(styles.text, styles.textHide)}>
+                {text}
+            </span>
+            <span className={styles.background} style={{ backgroundColor: color }} />
+            <span className={clsx(styles.text, styles.textAbsolute)} style={{ color }}>
+                {text}
+            </span>
+        </span>
+    );
 };
