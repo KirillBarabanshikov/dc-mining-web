a2d4da059a01ac66b62e24f15c8f5bec3252d209
Kirill <kirillbar24@gmail.com>
2024-08-07 02:09:21 +0400

    fix product card

--- src/entities/product/ui/ProductCard/ProductCard.tsx
+++ src/entities/product/ui/ProductCard/ProductCard.tsx
@@ -23,81 +23,87 @@ export const ProductCard: FC<IProductCardProps> = ({ product, viewMode = 'tile' 
     const matches = useMediaQuery(MAX_WIDTH_MD);
 
     return (
-        <article className={clsx(styles.productCard, styles[viewMode])} onClick={() => navigate('/product')}>
-            <div className={styles.image}>
-                <img src={BASE_URL + `${product.images[currentSlide].image}`} alt={'product'} />
-                <div className={styles.slides}>
-                    {product.images.map((image, index) => {
-                        return (
-                            <span key={image.id} onMouseEnter={() => setCurrentSlide(index)} className={styles.slide} />
-                        );
-                    })}
-                </div>
-                <div className={styles.pagination}>
-                    {product.images.slice(0, 4).map((image, index) => {
-                        return (
-                            <span
-                                key={image.id}
-                                className={clsx(styles.bullet, index === currentSlide && styles.active)}
-                            />
-                        );
-                    })}
-                </div>
-            </div>
-            <div className={styles.body}>
-                <div className={clsx(styles.wrap, styles.info)}>
-                    <div className={styles.tags}>
-                        {product.tags.map((tag) => {
-                            return <Badge key={tag.id} text={tag.title} color={tag.color} />;
+        <>
+            <article className={clsx(styles.productCard, styles[viewMode])} onClick={() => navigate('/product')}>
+                <div className={styles.image}>
+                    <img src={BASE_URL + `${product.images[currentSlide].image}`} alt={'product'} />
+                    <div className={styles.slides}>
+                        {product.images.map((image, index) => {
+                            return (
+                                <span
+                                    key={image.id}
+                                    onMouseEnter={() => setCurrentSlide(index)}
+                                    className={styles.slide}
+                                />
+                            );
                         })}
                     </div>
-                    {(viewMode === 'tile' || matches) && (
-                        <p className={styles.price}>{formatter.format(product.price)}</p>
-                    )}
-                    <p className={styles.name}>{product.title}</p>
-                    <div className={styles.specifications}>
-                        {product.value.map((value) => {
+                    <div className={styles.pagination}>
+                        {product.images.slice(0, 4).map((image, index) => {
                             return (
-                                <div key={value.id}>
-                                    {value.valueInKey} — {value.title} {value.unitInKey}
-                                </div>
+                                <span
+                                    key={image.id}
+                                    className={clsx(styles.bullet, index === currentSlide && styles.active)}
+                                />
                             );
                         })}
                     </div>
                 </div>
-                <div className={clsx(styles.wrap, styles.buttonsWrap)}>
-                    {!matches && viewMode === 'simple' && <p className={styles.price}>{product.price}</p>}
-                    <div className={styles.buttons}>
-                        <Button
-                            size={'sm'}
-                            className={styles.button}
-                            onClick={(e) => {
-                                e.stopPropagation();
-                                setIsOpen(true);
-                            }}
-                        >
-                            Заказать
-                        </Button>
-                        <IconButton
-                            icon={<HeartIcon />}
-                            onClick={(e) => {
-                                e.stopPropagation();
-                                setIsFavorite(!isFavorite);
-                            }}
-                            className={clsx(styles.iconButton, isFavorite && styles.isFavorite)}
-                        />
-                        <IconButton
-                            icon={<StatisticIcon />}
-                            onClick={(e) => {
-                                e.stopPropagation();
-                                setIsFavorite(!isFavorite);
-                            }}
-                            className={clsx(styles.iconButton, isFavorite && styles.isFavorite)}
-                        />
+                <div className={styles.body}>
+                    <div className={clsx(styles.wrap, styles.info)}>
+                        <div className={styles.tags}>
+                            {product.tags.map((tag) => {
+                                return <Badge key={tag.id} text={tag.title} color={tag.color} />;
+                            })}
+                        </div>
+                        {(viewMode === 'tile' || matches) && (
+                            <p className={styles.price}>{formatter.format(product.price)}</p>
+                        )}
+                        <p className={styles.name}>{product.title}</p>
+                        <div className={styles.specifications}>
+                            {product.value.map((value) => {
+                                return (
+                                    <div key={value.id}>
+                                        {value.valueInKey} — {value.title} {value.unitInKey}
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                    <div className={clsx(styles.wrap, styles.buttonsWrap)}>
+                        {!matches && viewMode === 'simple' && <p className={styles.price}>{product.price}</p>}
+                        <div className={styles.buttons}>
+                            <Button
+                                size={'sm'}
+                                className={styles.button}
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    setIsOpen(true);
+                                }}
+                            >
+                                Заказать
+                            </Button>
+                            <IconButton
+                                icon={<HeartIcon />}
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    setIsFavorite(!isFavorite);
+                                }}
+                                className={clsx(styles.iconButton, isFavorite && styles.isFavorite)}
+                            />
+                            <IconButton
+                                icon={<StatisticIcon />}
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    setIsFavorite(!isFavorite);
+                                }}
+                                className={clsx(styles.iconButton, isFavorite && styles.isFavorite)}
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </article>
             <OrderProductModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
-        </article>
+        </>
     );
 };
