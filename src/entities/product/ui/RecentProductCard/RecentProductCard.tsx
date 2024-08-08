fd7dcf5df32b5d7aa54d22ab23ca58d9eb54dc96
Kirill <kirillbar24@gmail.com>
2024-08-08 22:37:47 +0400

    fix recent product card

--- src/entities/product/ui/RecentProductCard/RecentProductCard.tsx
+++ src/entities/product/ui/RecentProductCard/RecentProductCard.tsx
@@ -1,9 +1,12 @@
+import { useNavigate } from 'react-router-dom';
 import img from '@/widgets/Bestsellers/img.png';
 import styles from './RecentProductCard.module.scss';
 
 export const RecentProductCard = () => {
+    const navigate = useNavigate();
+
     return (
-        <article className={styles.card}>
+        <article className={styles.card} onClick={() => navigate('/product')}>
             <img src={`${img}`} alt={'Product'} />
             <div>
                 <p className={styles.name}>Asic майнер Bitmain Antminer S19K PRO 115 TH/s</p>
