3f6f22fc15f1c8c660ca3e7d78120f8a48a283d7
Kirill <kirillbar24@gmail.com>
2024-08-15 13:44:31 +0300

    added news list on main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,13 @@
 import clsx from 'clsx';
-import { Bestsellers, MainBanner, Managers, Offers } from '@/widgets';
+import { Button } from '@/shared/ui';
+import { Bestsellers, MainBanner, Managers, NewsList, Offers } from '@/widgets';
 import Background from '@/shared/assets/backgrounds/main-bg.svg?react';
 import styles from './MainPage.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 const MainPage = () => {
+    const navigate = useNavigate();
+
     return (
         <>
             <MainBanner />
@@ -14,9 +18,18 @@ const MainPage = () => {
                     <Bestsellers />
                     <Background className={styles.backgroundIcon} />
                 </div>
-            </div>
-            <div className={'container'}>
                 <Managers />
+                <section className={styles.news}>
+                    <div className={'container'}>
+                        <div className={styles.titleWrap}>
+                            <h2 className={'section-title'}>Новости и статьи</h2>
+                            <Button variant={'outline'} onClick={() => navigate('/news')}>
+                                Все новости
+                            </Button>
+                        </div>
+                        <NewsList />
+                    </div>
+                </section>
             </div>
         </>
     );
