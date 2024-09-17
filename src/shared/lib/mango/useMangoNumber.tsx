549e06eb8a92b3c1cd5981c6d3c23f2d851090e9
Kirill <kirillbar24@gmail.com>
2024-09-17 15:01:25 +0400

    fix

--- src/shared/lib/mango/useMangoNumber.tsx
+++ src/shared/lib/mango/useMangoNumber.tsx
@@ -2,14 +2,14 @@ import { useEffect } from 'react';
 
 export const useMangoNumber = () => {
     useEffect(() => {
-        if (typeof mgo === 'undefined') return;
-
-        mgo(function (mgo: any) {
-            mgo?.getNumber('', function (result: any) {
-                document.querySelectorAll('.mgo-number').forEach(function (elem: any) {
-                    elem.innerText = result.number;
-                });
-            });
-        });
+        // if (typeof mgo === 'undefined') return;
+        //
+        // mgo(function (mgo: any) {
+        //     mgo?.getNumber('', function (result: any) {
+        //         document.querySelectorAll('.mgo-number').forEach(function (elem: any) {
+        //             elem.innerText = result.number;
+        //         });
+        //     });
+        // });
     }, []);
 };
