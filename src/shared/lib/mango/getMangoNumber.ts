34be2f33a4da663e3ed851e23e9a1c1264d1ecc3
Kirill <kirillbar24@gmail.com>
2024-09-17 16:28:19 +0400

    fix

--- src/shared/lib/mango/getMangoNumber.ts
+++ src/shared/lib/mango/getMangoNumber.ts
@@ -5,12 +5,12 @@ export const getMangoNumber = () => {
         console.log('test', test);
 
         // Запрашиваем номер
-        // mgo?.getNumber('', function (result: any) {
-        //     // Вставляем номер телефона во все теги с классом mgo-number
-        //     document.querySelectorAll('.mgo-number').forEach(function (elem: any) {
-        //         console.log(result);
-        //         elem.innerText = result.number;
-        //     });
-        // });
+        mgo?.getNumber('', function (result: any) {
+            // Вставляем номер телефона во все теги с классом mgo-number
+            document.querySelectorAll('.mgo-number').forEach(function (elem: any) {
+                console.log('result', result);
+                elem.innerText = result.number;
+            });
+        });
     });
 };
