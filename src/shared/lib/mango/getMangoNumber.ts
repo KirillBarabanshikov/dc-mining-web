b1c16f565ae0f5b7734073f08abaeb493e6e67e6
Kirill <kirillbar24@gmail.com>
2024-09-17 16:42:45 +0400

    fix

--- src/shared/lib/mango/getMangoNumber.ts
+++ src/shared/lib/mango/getMangoNumber.ts
@@ -1,7 +1,13 @@
 export const getMangoNumber = () => {
+    console.log('mgo', mgo);
+
     mgo(function (mg: any) {
+        console.log('test', mg);
+
         mg?.getNumber('', function (result: any) {
+            console.log('result', result);
             window.phone = result.number;
+            console.log('phone', result.number);
         });
     });
 };
